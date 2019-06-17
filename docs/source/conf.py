a31f2a08a0a41eebf1b1bc63baca6ba3471e795d
jsuarez5341 <sealsuarez@gmail.com>
2019-06-17 16:39:00 -0700

    mock pytorch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,16 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['torch']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 

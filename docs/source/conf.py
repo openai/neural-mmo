8b4c66f751ca5f800da5bc1756fa8ae41cb39dde
jsuarez5341 <sealsuarez@gmail.com>
2019-06-17 17:05:47 -0700

    +mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['torch', 'torch.autograd', 'torch.nn', 'torch.distributions']
+MOCK_MODULES = ['torch', 'torch.autograd', 'torch.nn', 'torch.distributions', 'torch.nn.parameter']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 

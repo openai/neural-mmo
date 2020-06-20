f81e4c57111dfaba18f0418a9b52b4616acce9a1
jsuarez5341 <sealsuarez@gmail.com>
2020-06-19 17:17:01 -0700

    Messy commit -- working from laptop for a bit. Beta testers use previous commit

--- quickplot.py
+++ quickplot.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import re
 
-dat = open('train.txt').read()
+dat = open('train_attn.txt').read()
 R   = []
 
 regex = re.compile('Lifetime: .*')
@@ -17,6 +17,7 @@ final = []
 for i in range(0, len(R), 6):
    final.append(np.mean(R[i:i+6]))
    
+#np.save('data_recur.npy', final)
 plt.plot(final, 'b', lw=2)
 plt.xlabel('Epochs')
 plt.ylabel('Lifetime')
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,6 +1,16 @@
 from pdb import set_trace as T
 import os
 
+#Install dependencies with Poetry (python 3.7.x)
+#You may also need to install gcc on a fresh OS
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python && source ~/.poetry/env && poetry install
+
+#Download the Neural MMO environment
+git clone https://github.com/jsuarez5341/neural-mmo && cd neural-mmo
+
+#Download the Neural MMO client -- not required for training
+git clone https://github.com/jsuarez5341/neural-mmo-client && mv neural-mmo-client forge/embyr
+
 #Install python requirements
 #print('Installing requirements')
 #os.system('pip install -r scripts/requirements.txt')

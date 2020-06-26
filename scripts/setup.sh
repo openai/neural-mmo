914365378cc9400dcf7162317f3bb02270c54b12
jsuarez5341 <sealsuarez@gmail.com>
2020-06-26 00:11:14 -0400

    requirements

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,25 @@
 echo "Running Neural MMO setup"
 echo "This may take a few minutes..."
+echo "If you are on a VM, install basic utilities first (curl, python, git)"
 
-if [$1 == "--SERVER_ONLY"]
-then
+if [[ $1 == "--SERVER_ONLY" ]]; then 
    echo "You have chosen not to install the graphical rendering client"
-else
+elif [[ $1 == "" ]]; then
    echo "Installing Neural MMO Client (Unity3D)..."
    git clone https://github.com/jsuarez5341/neural-mmo-client
    mv neural-mmo-client forge/embyr
+else
+   echo "Specify either --SERVER_ONLY or no argument"
+   exit 1
 fi
 
 echo "Installing Poetry to manage dependencies..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 source ~/.poetry/env
+poetry config virtualenvs.create false
 
 echo "Installing dependencies with Poetry..."
 poetry install
 
 echo "Generating game maps. This may take a few minutes..."
-python -c "import terrain"
+python -c "from scripts import terrain"
---
+++ scripts/vm.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git, vim, curl, python3-is-python, python3-distutils python3-venv gcc python3-dev python3-pip

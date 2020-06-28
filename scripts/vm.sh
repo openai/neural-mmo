ab1d44023dba9fba8af40b9344ba100c14f1f49e
jsuarez5341 <sealsuarez@gmail.com>
2020-06-27 22:12:41 -0400

    Working on 20.04

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -4,6 +4,7 @@
 #sudo apt install python3.7
 #sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 10
 
-sudo apt-get install curl gcc python3-pip
+sudo apt install curl gcc python3-pip
+sudo apt install python-is-python3
 #sudo apt-get install python3-distutils python3-venv python3-dev python3-pip
 #sudo apt-get install python-is-python3 #Rename python=python3 on 20.0

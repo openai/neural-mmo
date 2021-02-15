42ba4f1c1f0252105752c3e3b32fef21a289b2fa
jsuarez5341 <sealsuarez@gmail.com>
2021-02-15 15:29:49 -0500

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-read -p "DEPENDENCIES: this script requires Anaconda Python 3.8, gcc, make, cmake, and build-essential. Installation will fail otherwise. Proceed [y/n]?" -n 1 -r
+read -p "DEPENDENCIES: this script requires Anaconda Python 3.8, gcc, zlib1g-dev, make, cmake, and build-essential. Installation will fail otherwise. Proceed [y/n]?" -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -8,6 +8,9 @@ fi
 #Install python packages
 echo "Installing conda pip packages"
 pip install -r scripts/requirements.txt
+
+echo "Installing rllib"
+pip install ray[rllib]
 
 echo "Installing cuda torch"
 conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch

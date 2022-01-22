ba82b8ded1a683a2014f5b944683f1124d44113d
jsuarez5341 <jsuarez@LAPTOP-FB8A0O6H.localdomain>
2022-01-21 16:30:37 -0800

    All config in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 from pdb import set_trace as T
+from itertools import chain
+
 from setuptools import find_packages, setup
+
 
 REPO_URL = "https://github.com/neuralmmo/environment"
 
+extra = {
+    'docs': [
+        'sphinx-rtd-theme==0.5.1',
+        'sphinxcontrib-youtube==1.0.1',
+        ],
+    'rllib': [
+        'ray[all]==1.5.2',
+        'tensorflow==2.7.0',
+        'grpcio==1.32.0',
+        'six==1.15.0',
+        'dm-tree==0.1.6',
+        'aiohttp==3.7',
+        'pandas==1.3.4',
+        'wandb==0.12.9',
+        'openskill==0.2.0-alpha.0',
+        'torch',
+        ],
+    }
+
+extra['all'] = list(set(chain.from_iterable(extra.values())))
+ 
 setup(
     name="nmmo",
     description="Neural MMO is a platform for multiagent intelligence research inspired by "
@@ -23,24 +47,7 @@ setup(
         'tqdm==4.61.1',
         'pettingzoo==1.14.0',
     ],
-    extras_require={
-        'docs': [
-            'sphinx-rtd-theme==0.5.1',
-            'sphinxcontrib-youtube==1.0.1',
-        ],
-        'rllib': [
-            'ray[all]==1.5.2',
-            'tensorflow==2.7.0',
-            'grpcio==1.32.0',
-            'six==1.15.0',
-            'dm-tree==0.1.6',
-            'aiohttp==3.7',
-            'pandas==1.3.4',
-            'wandb==0.12.9',
-            'openskill==0.2.0-alpha.0',
-            'torch',
-        ],
-    },
+    extras_require=extra,
     python_requires=">=3.9",
     license="MIT",
     author="Joseph Suarez",

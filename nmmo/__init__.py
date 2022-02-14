09adf6f1e4f0f2d47af0a55887e27f74b8fd33c3
jsuarez5341 <sealsuarez@gmail.com>
2022-02-14 17:33:04 -0500

    Fix ascii motd path

--- nmmo/__init__.py
+++ nmmo/__init__.py
@@ -1,7 +1,17 @@
 from .version import __version__
 
 import os
-motd = open(os.path.dirname(__file__) + '/resource/ascii.txt').read().format(__version__)
+motd = '''      ___           ___           ___           ___
+     /__/\         /__/\         /__/\         /  /\      Version {:<8}
+     \  \:\       |  |::\       |  |::\       /  /::\ 
+      \  \:\      |  |:|:\      |  |:|:\     /  /:/\:\    An open source
+  _____\__\:\   __|__|:|\:\   __|__|:|\:\   /  /:/  \:\   project originally
+ /__/::::::::\ /__/::::| \:\ /__/::::| \:\ /__/:/ \__\:\  founded by Joseph Suarez
+ \  \:\~~\~~\/ \  \:\~~\__\/ \  \:\~~\__\/ \  \:\ /  /:/  and formalized at OpenAI
+  \  \:\  ~~~   \  \:\        \  \:\        \  \:\  /:/
+   \  \:\        \  \:\        \  \:\        \  \:\/:/    Now developed and
+    \  \:\        \  \:\        \  \:\        \  \::/     maintained at MIT in
+     \__\/         \__\/         \__\/         \__\/      Phillip Isola's lab '''.format(__version__)
 
 from . import scripting
 from .lib import material
@@ -19,3 +29,4 @@ from .core.terrain import MapGenerator, Terrain
 __all__ = ['Env', 'config', 'scripting', 'agent', 'Agent', 'MapGenerator', 'Terrain',
         'Serialized', 'action', 'Action', 'scripting', 'material',
         'Task', 'Overlay', 'OverlayRegistry']
+

c49afa260bfcfa23343906c3739d527c6e573b68
jsuarez5341 <jsuarez@LAPTOP-FB8A0O6H.localdomain>
2022-03-04 15:34:41 -0800

    Fix circular dep

--- nmmo/io/serial.py
+++ nmmo/io/serial.py
@@ -2,11 +2,10 @@ from pdb import set_trace as T
 import numpy as np
 
 import nmmo
-from nmmo.io.stimulus import Serialized
 
 def batch(obs):
     batched = {}
-    for (entity_name,), entity in Serialized:
+    for (entity_name,), entity in nmmo.io.stimulus.Serialized:
         batched[entity_name] = {}
         for dtype in 'Continuous Discrete N'.split():
             attr_obs = [obs[k][entity_name][dtype] for k in obs]
@@ -29,7 +28,7 @@ def pack(obs):
 def unpack(config, packed_obs):
     obs, idx = {}, 0
     batch = len(packed_obs)
-    for (entity_name,), entity in Serialized:
+    for (entity_name,), entity in nmmo.io.stimulus.Serialized:
         n_entity = entity.N(config)
         n_continuous, n_discrete = 0, 0
         obs[entity_name] = {}
@@ -55,9 +54,9 @@ def unpack(config, packed_obs):
     return obs
 
 def equals(batch1, batch2):
-   entity_keys = [e[0][0] for e in Serialized]
+   entity_keys = [e[0][0] for e in nmmo.io.stimulus.Serialized]
    assert list(batch1.keys()) == list(batch2.keys()) == entity_keys
-   for (entity_name,), entity in Serialized:
+   for (entity_name,), entity in nmmo.io.stimulus.Serialized:
         #attribute_keys = [e[0] for e in entity]
 
         batch1_attrs = batch1[entity_name]
@@ -69,12 +68,14 @@ def equals(batch1, batch2):
         for key in attr_keys:
             assert np.array_equal(batch1_attrs[key], batch2_attrs[key])
 
-class Config(nmmo.config.Medium):
-    @property
-    def SPAWN(self):
-        return self.SPAWN_CONCURRENT
-
 def setup():
+    import nmmo
+
+    class Config(nmmo.config.Medium):
+        @property
+        def SPAWN(self):
+            return self.SPAWN_CONCURRENT
+
     env = nmmo.Env(Config())
     obs = env.reset()
     return env, obs

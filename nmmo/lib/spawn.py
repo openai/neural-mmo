a65e2587a1bd77c4a7440d87e322534621066d4b
jsuarez5341 <sealsuarez@gmail.com>
2022-08-12 22:40:40 -0400

    Fix team loader bug

--- nmmo/lib/spawn.py
+++ nmmo/lib/spawn.py
@@ -23,7 +23,7 @@ class TeamLoader:
     '''config.PLAYER_LOADER that loads agent populations adjacent'''
     def __init__(self, config):
         items = config.PLAYERS
-        self.team_size = config.PLAYER_N // config.NPOP
+        self.team_size = config.PLAYER_N // len(items)
 
         for idx, itm in enumerate(items):
            itm.policyID = idx 
